4212768832ec23e6b2db20831765aad60ad92173
noobExtendsBot <hackerearth02@gmail.com>
2023-07-06 17:45:41 +0100

    added new comments

--- scripts/MyClasses.py
+++ scripts/MyClasses.py
@@ -13,6 +13,7 @@ class Terminal:
 
 class Telementry:
     def __init__(self, terminals, longitude, latitude):
+        # terminals will be a list
         self.created_at = fake.date_time_this_decade()
         self.terminals = terminals
         self.longitude = longitude
@@ -33,41 +34,43 @@ def generate_fake_telementry(terminals):
     return Telementry(terminals, longitude, latitude)
 
 
-# Generate fake data for 10 people
-people = [fake.name() for _ in range(10)]
+if __name__ == "__main__":
+    # Generate fake data for 10 people or Terminal (treating each as Person to user fake.name())
+    people = [fake.name() for _ in range(10)]
 
-telementry_list = []
-terminals = []
+    telementry_list = []
+    # A list so that Telementry object can have multiple Terminal for the same object
+    terminals = []
 
-for person in people:
-    terminal = Terminal(person)
-    terminals.append(terminal)
+    for person in people:
+        terminal = Terminal(person)
+        terminals.append(terminal)
 
-    telementry_list.append(generate_fake_telementry([terminal]))
+        telementry_list.append(generate_fake_telementry([terminal]))
 
-# Calculate average latitude and longitude for each unique Terminal name
-terminal_data = defaultdict(list)
-northern_terminals = []
+    # Calculate average latitude and longitude for each unique Terminal name
+    terminal_data = defaultdict(list)
+    northern_terminals = []
 
-for telementry in telementry_list:
-    for terminal in telementry.terminals:
-        terminal_data[terminal.name].append((telementry.longitude, telementry.latitude))
+    for telementry in telementry_list:
+        for terminal in telementry.terminals:
+            terminal_data[terminal.name].append((telementry.longitude, telementry.latitude))
 
-        if telementry.latitude >= 0:
-            northern_terminals.append(terminal.name)
+            if telementry.latitude >= 0:
+                northern_terminals.append(terminal.name)
 
-# Print average latitude and longitude for each unique Terminal name
-for name, coordinates in terminal_data.items():
-    longitudes, latitudes = zip(*coordinates)
-    average_longitude = sum(longitudes) / len(longitudes)
-    average_latitude = sum(latitudes) / len(latitudes)
+    # Print average latitude and longitude for each unique Terminal name
+    for name, coordinates in terminal_data.items():
+        longitudes, latitudes = zip(*coordinates)
+        average_longitude = sum(longitudes) / len(longitudes)
+        average_latitude = sum(latitudes) / len(latitudes)
 
-    print(f"Terminal Name: {name}")
-    print(f"Average Longitude: {average_longitude}")
-    print(f"Average Latitude: {average_latitude}")
-    print()
+        print(f"Terminal Name: {name}")
+        print(f"Average Longitude: {average_longitude}")
+        print(f"Average Latitude: {average_latitude}")
+        print()
 
-# Print Terminals in the Northern Hemisphere
-print("Terminals in the Northern Hemisphere:")
-for name in set(northern_terminals):
-    print(name)
+    # Print Terminals in the Northern Hemisphere
+    print("Terminals in the Northern Hemisphere:")
+    for name in set(northern_terminals):
+        print(name)
